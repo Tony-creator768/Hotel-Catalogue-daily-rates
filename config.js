f08abc00411bc6c98ae7410d4f23e25687c8208f
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-06-10 13:43:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Paste your Google Apps Script Web App URL below after deploying Code.gs.
 // Leave DATA_URL as the placeholder to preview using sample-data.js.
 window.CATALOGUE_CONFIG = {
-  DATA_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwB0509xjS7gULmesDg6-a_UfJeXGAM7N0uFgn8iZ0dyOAp5mZuYp8wp_-rAfJanNP_Mg/exec",
   APPROVED_ONLY: true,
   SHOW_INTERNAL_NOTES: false,
   CURRENCY: "QAR",
